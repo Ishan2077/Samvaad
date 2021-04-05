55e42b97a3d02edb73bcf11e40fff6531ddd3f8f
Ishan Sharma <ishan.sharma3355@gmail.com>
2021-04-06 00:47:26 +0530

    Legal Section updated.

--- src/pages/PageLegal.vue
+++ src/pages/PageLegal.vue
@@ -2,8 +2,24 @@
 <template>
   <q-page class="q-pa-lg">
     <h4 class="q-mt-none q-mb-md text-weight-bold">Legal</h4>
-    <div class="text-body1">
-    <p>Legal . . .</p>
+    <div class="text-weight-bold text-justify">
+    <p>
+      Let's not take it to court. Of course, you won't :p
+      <br><br>
+       <div class="q-pa-md">
+    <div class="q-col-gutter-md row items-start">
+      <div class="col-6">
+        <q-img
+          src="https://image.spreadshirtmedia.com/image-server/v1/compositions/103723715/views/1,width=300,height=300,appearanceId=6,version=1478258753/weed-y-u-no-legal-men-s-hoodie.jpg"
+          style="width: 100%"
+        >
+          <div class="absolute-full text-subtitle1 flex flex-center">
+            No legal things please!
+          </div>
+        </q-img>
+      </div>
+    </div>
+       </div>
     </div>
   </q-page>
 </template>
