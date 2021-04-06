d1c732c01aed442822ebbc7f2e631a44ace48742
Ishan Sharma <ishan.sharma3355@gmail.com>
2021-04-06 19:20:30 +0530

    Content formatted in different sections. Forms implementation started, ver 1.0

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -3,18 +3,22 @@
   <q-page class="q-pa-lg">
     <h4 class="q-mt-none q-mb-md text-weight-bold">About Samvaad</h4>
     <div class="text-body1">
-    <p><b>Samvaad</b> - A <b>Made In India</b> initiative that brings users together. You can share, communicate, catch up with the lastest news and do
+    <p><b>Samvaad</b> - A <b>Made In India</b> initiative that brings everyone together. You can share, communicate, catch up with the lastest news and do
       much more besides. <br>
-      We bring new ideas and make communication more accessible than ever before!
+      We bring novel ideas to life and make communication more accessible than ever before!
     </p>
     <h5 class="q-mt-none q-mb-md text-weight-bold">Features</h5>
-    <li>Share </li>
-    <li>Communicate</li>
-    <li>Direct Messages</li>
-    <li>Current News</li>
-    <li>More features being added!</li>
+    <ul class="list-styles">
+      <li>Share </li>
+      <li>Communicate</li>
+      <li>Direct Messages</li>
+      <li>Current News</li>
+      <li>More features being added!</li>
+    </ul>
     <br>
-    <p>As we continue to develop this application for you, we want our users to about how they feel about the overall experience and how we can shape this experience into something truly spectacular. We take all feedback with utmost attention.
+    <p>As we continue to develop this application for you, we want you to tell us about the overall experience and how we can shape this experience into something truly spectacular.
+      <br>
+       We take all feedback(s) with utmost attention and strive to change the way we communicate.
     </p>
     </div>
   </q-page>
@@ -25,3 +29,9 @@ export default {
   name: 'PageHome'
 }
 </script>
+
+<style scoped>
+ul.list-styles{
+   list-style-type: disc;
+}
+</style>
--- src/pages/PageContact.vue
+++ src/pages/PageContact.vue
@@ -2,13 +2,42 @@
   <q-page class="q-pa-sm">
     <h4 class="q-mt-none q-mb-md text-weight-bold">Contact Us</h4>
     <div class="text-body1">
-    <p>For any queries or complaints, write us to at:
-    <b>samvaad@gmail.com</b>
-    .We will be happy to assist you!<br><br>
-    <i>For general suggestions and more, kindly refer to the <b>Mouthoff</b> section.</i></p>
+    <p>
+      For any queries or complaints, kindly fill this form. We will look into your feedback and get back to you shortly!
+    <br>
+    </p>
+    <div class="q-pa-md">
+      <div class="q-gutter-md" style="max-width: 300px">
+        <q-input v-model="text" label="Your Name *" required/>
+        <q-input v-model="text" label="Email Address *" required type="email"/>
+        <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="Kindly enter your password(To check it's you :p)">
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+         <q-input
+            v-model="text"
+            filled
+            type="textarea"
+          />
+      </div>
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-file color="grey-6" v-model="model" label="Add file(s)">
+        <template v-slot:prepend>
+          <q-icon name="attach_file" />
+        </template>
+      </q-file>
+    </div>
+    </div>
+    <footer>For general suggestions and more, kindly refer to the <b>Mouthoff</b> section.</footer>
     </div>
   </q-page>
 </template>
+
 <script>
 export default {
   name: 'PageHome'
