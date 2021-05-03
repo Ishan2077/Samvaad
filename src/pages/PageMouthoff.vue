995e659fbaaa6884bb5296a4612d3a09b522c171
Ishan Sharma <ishan.sharma3355@gmail.com>
2021-05-03 17:19:40 +0530

    Misc bug fixes.

--- src/pages/PageMouthoff.vue
+++ src/pages/PageMouthoff.vue
@@ -2,33 +2,19 @@
   <q-page class="q-pa-sm">
     <h4 class="q-mt-none q-mb-md text-weight-bold">Mouthoff</h4>
     <div class="text-body1">
-      <q-banner rounded class="bg-red-6 text-black">
-        We are facing a large number of requests. Please wait for us to respond within 7 business days.
-      </q-banner>
+      <q-banner class="bg-yellow-4">
+        <template v-slot:avatar>
+          <q-icon name="online_prediction" color="dark" />
+        </template>
+        <div class="text-weight-bold">
+          We are receiving huge number of requests at the moment.<br> Kindly wait for 7 business days for our Mouthoff Support Executive to reach out to you. <br>Sorry for the inconvenience :(
+        </div>
+    </q-banner>
       <br>
       <p class="text-deep-purple">The official way to stay even more connected! Friends, Right? :)</p>
       <p> Anything you want us to tell, be it your feedback, to request a feature, some jokes or any general suggestion on how we can improve, please drop an e-mail on the following address:<br>
       <i>Please note that any Support service(s) will <b>not</b> be entertained through this section. Kindly visit the <b>Contact</b> page for more details.</i> </p>
-     <!-- <h6 class="q-mt-none">Thank you so much for your support! Hope we receive some cool things :')</h6> -->
-    </div>
-    <!--<div class="q-pa-md" style="max-width: 300px">
-      <div class="q-gutter-md text-weight-bold">
-        <q-select v-model="model" :options="options" option-label="choose" label="Sup*? :)" :required="true"/>
-        <q-file filled bottom-slots v-model="model" label="Label" counter>
-        <template v-slot:prepend>
-          <q-icon name="cloud_upload" @click.stop />
-        </template>
-        <template v-slot:append>
-          <q-icon name="close" @click.stop="model = null" class="cursor-pointer" />
-        </template>
-
-        <template v-slot:hint>
-          Field hint
-        </template>
-      </q-file>
       </div>
-    </div>
-    -->
   </q-page>
 </template>
 <script>
